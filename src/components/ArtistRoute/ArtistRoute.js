e3322d56b612443d362a87da1b9aca5a96ed6660
Roger Lam <rogerlam@wylco.org>
2020-09-30 13:37:18 -0400

    add style

--- src/components/ArtistRoute/ArtistRoute.js
+++ src/components/ArtistRoute/ArtistRoute.js
@@ -50,12 +50,15 @@ const ArtistRoute = () => {
       }
       {currentArtist &&
         <Wrapper>
-          <img src={currentArtist.images[0].url} />
-          <h1>{currentArtist.name}</h1>
-          <h2>Followers</h2>
-          <p>{roundNumbers(currentArtist.followers.total)}</p>
-          <h2>Tags</h2>
-          <p>{currentArtist.genres[0]}, {currentArtist.genres[1]}</p>
+          <ArtistTitleWrapper>
+            <ArtistImg src={currentArtist.images[0].url} alt={"Image for " + currentArtist.name} />
+            <ArtistName>{currentArtist.name}</ArtistName>
+          </ArtistTitleWrapper>
+          <ArtistInfoWrapper>
+            <p><FollowerNumHighlight>{roundNumbers(currentArtist.followers.total)}</FollowerNumHighlight> followers</p>
+            <h2>tags</h2>
+            <p><TagWrapper>{currentArtist.genres[0]}</TagWrapper><TagWrapper>{currentArtist.genres[1]}</TagWrapper></p>
+          </ArtistInfoWrapper>
         </Wrapper>
       }
     </>
@@ -65,5 +68,47 @@ const ArtistRoute = () => {
 export default ArtistRoute;
 
 const Wrapper = styled.div`
+  width: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #0B0F14;
+  color: white;
+  padding: 60px;
+  font-weight: bold;
+  height: 100vh;
+`
 
+const ArtistTitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const ArtistName = styled.p`
+  margin-top: -60px;
+  font-size: 40px;
+`
+const ArtistImg = styled.img`
+  border-radius: 50%;
+  width: 175px;
+  height: 175px;
+  top: 25px;
+`
+
+const FollowerNumHighlight = styled.span`
+  color: #FF4FD8;
+`
+
+const ArtistInfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const TagWrapper = styled.span`
+  padding: 10px;
+  margin: 5px;
+  background: rgba(75, 75, 75, 0.4);
+  border-radius: 4px;
 `
